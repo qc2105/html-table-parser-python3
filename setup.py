8576558e0b2ea0ea79cf4bf303f74baf361dfc90
ahobsonsayers <32173585+ahobsonsayers@users.noreply.github.com>
2018-07-30 19:53:32 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='html_table_parser_python3',
+      version='1.0',
+      description='Parses HTML Tables',
+      url='https://github.com/ahobsonsayers/html-table-parser-python',
+      author='Arran Hobson Sayers',
+      author_email='ahobsonsayers@gmail.com',
+      license='GPL',
+      packages=['html_table_parser_python3'],
+      zip_safe=False)
